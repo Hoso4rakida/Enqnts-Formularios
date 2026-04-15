e981694cddba6d270d4e50749da10aaa637e690d
Hoso4rakida <joedavid2003@proton.me>
2026-04-15 13:04:15 -0300

    fix: agora lê enquete

--- microservice/jsonRead.tsx
+++ microservice/jsonRead.tsx
@@ -2,7 +2,7 @@
 export default async function LerJSON(id: string) {
 
   const URL_BASE = process.env.NEXT_PUBLIC_BLOB_URL; 
-  const caminhoNoBlob = `${URL_BASE}${id}.json`;
+  const caminhoNoBlob = `${URL_BASE}/enquetes/${id}.json`;
 
   try {
     const response = await fetch(caminhoNoBlob, {
